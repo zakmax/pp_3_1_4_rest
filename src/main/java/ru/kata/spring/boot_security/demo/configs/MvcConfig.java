3efb8dc8e5be9a69fd0483e91816501f570ab45f
zak-max15@yandex.ru <java180490>
2025-12-16 12:49:17 +0300

    подчистил последнее

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -9,16 +9,16 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // Основные страницы
+
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/user").setViewName("user");
 
-        // Редиректы
+
         registry.addRedirectViewController("/", "/login");
         registry.addRedirectViewController("", "/login");
 
-        // API документация (опционально)
+
         registry.addViewController("/api/docs").setViewName("api-docs");
     }
 }
